28471fee1481772bfd10437fe12fee5af1eb8635
nazim uddin <mdnajim1247@gmail.com>
2025-07-17 20:24:20 +0600

    daisiuy setup done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Home Page</h1>
+      <Footer />
     </div>
   );
 }
