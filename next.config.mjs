77ea45d4fee3edd8ec05a6530efcadd691516979
nazim uddin <mdnajim1247@gmail.com>
2025-08-11 15:30:28 +0600

    created new resuts

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,11 @@ const nextConfig = {
             hostname: 'lh3.googleusercontent.com',
             
         },
+        {
+            protocol: 'https',
+            hostname: 'i.ibb.co.com',
+            
+        },
     ],
   },
   
